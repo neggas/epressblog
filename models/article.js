9cb4ddc8a984798e974eee688e4d3e58dda021e4
rolls98 <toureb16@gmail.com>
2020-03-21 01:53:21 +0100

    Ajout du back Admin

--- models/article.js
+++ models/article.js
@@ -3,20 +3,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ArticleSchema = new Schema({
-
-    titre: {
-        type: String,
-        required: true
-    },
-    article: {
-        type: String,
-        required: true
-    },
-    comments: {
-        type: Object,
-        required: false
-    }
-
-})
+  titre: {
+    type: String,
+    required: true
+  },
+  article: {
+    type: String,
+    required: true
+  },
+  comments: {
+    type: Object,
+    required: false
+  }
+});
 
 module.exports = mongoose.model("Article", ArticleSchema);
