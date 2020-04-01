b2d02428a4a8e69b0ea3d73de7ce352a1c659d6f
rolls98 <toureb16@gmail.com>
2020-04-01 20:48:21 +0200

    ajout d'image pour les articles et correction du dashboard

--- models/article.js
+++ models/article.js
@@ -3,18 +3,32 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ArticleSchema = new Schema({
-    titre: {
-        type: String,
-        required: true
-    },
-    article: {
-        type: String,
-        required: true
-    },
-    comments: {
-        type: Object,
-        required: false
-    }
+  titre: {
+    type: String,
+    required: true
+  },
+  article: {
+    type: String,
+    required: true
+  },
+  likes: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  views: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  comments: {
+    type: Object,
+    required: false
+  }
 });
 
 module.exports = mongoose.model("Article", ArticleSchema);
