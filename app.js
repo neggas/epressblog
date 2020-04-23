d4d4383c4e5e481f094e0ef66151790089467ad9
neggas <arsene.fop@gmail.com>
2020-04-23 19:38:20 +0200

    preparation au deployement

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ const path = require("path");
 const mongoConnect = require("./utils/database");
 const mongoose = require("mongoose");
 const flash = require("connect-flash");
+const morgan = require("morgan");
 const session = require("express-session");
+const helmet = require("helmet");
+const fs = require("fs");
+const compression = require("compression");
 const MongoDbStore = require("connect-mongodb-session")(session);
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
 
@@ -34,20 +39,15 @@ app.use((req, res, next) => {
 
 const authRoute = require("./router/auth");
 const articleRoute = require("./router/articles");
-
+const logStream = fs.createWriteStream(path.join(__dirname, 'accessLog'), { flags: 'a' });
 app.use(flash());
 app.use(authRoute);
 app.use("/blog", articleRoute);
+app.use(compression());
+app.use(helmet());
+app.use(morgan("combined", { stream: logStream }));
+
 const PORT = process.env.PORT || 3000;
-// mongoose
-//     .connect("mongodb://localhost/blog", { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true })
-//     .then(response => {
-//         const PORT = process.env.PORT || 3000;
-//         app.listen(PORT, () => {
-//             console.log(`le serveur ecoute sur le port ${PORT}`);
-//         });
-//     })
-//     .catch(err => console.log(`erreur : ${err}`));
 
 mongoConnect((client) => {
     app.listen(PORT, () => {
--- utils/database.js
+++ utils/database.js
@@ -3,7 +3,7 @@ const MongoClient = require('mongoose');
 
 const mongoConnect = (callback) => {
 
-    MongoClient.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0-d57uy.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true/`, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true })
+    MongoClient.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0-shard-00-00-d57uy.mongodb.net:27017,cluster0-shard-00-01-d57uy.mongodb.net:27017,cluster0-shard-00-02-d57uy.mongodb.net:27017/${process.env.MONGO_DEFAULT_DATABASE}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true`, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true })
         .then((result) => {
             callback(result)
         })
