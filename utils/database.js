348c7ca7bb15101c3959bd00fbf51b9557d29df0
neggas <arsene.fop@gmail.com>
2020-04-23 17:10:54 +0200

    Utilisation de variable d'environnement

--- utils/database.js
+++ utils/database.js
@@ -2,7 +2,8 @@ const MongoClient = require('mongoose');
 
 
 const mongoConnect = (callback) => {
-    MongoClient.connect('mongodb+srv://Arsene:nVBETDaXtkAhKRvK@cluster0-d57uy.mongodb.net/blog?retryWrites=true/', { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true })
+
+    MongoClient.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0-d57uy.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true/`, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true })
         .then((result) => {
             callback(result)
         })
