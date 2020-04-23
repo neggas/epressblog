3aa5550cf52f7461a8530ce887b01efe4ecb9ba8
neggas <arsene.fop@gmail.com>
2020-04-23 20:27:55 +0200

    tentative de reglage de la db

--- utils/database.js
+++ utils/database.js
@@ -3,7 +3,14 @@ const MongoClient = require('mongoose');
 
 const mongoConnect = (callback) => {
 
-    MongoClient.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0-shard-00-00-d57uy.mongodb.net:27017,cluster0-shard-00-01-d57uy.mongodb.net:27017,cluster0-shard-00-02-d57uy.mongodb.net:27017/${process.env.MONGO_DEFAULT_DATABASE}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true`, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true })
+    MongoClient.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0-shard-00-00-d57uy.mongodb.net:27017,cluster0-shard-00-01-d57uy.mongodb.net:27017,cluster0-shard-00-02-d57uy.mongodb.net:27017/${process.env.MONGO_DEFAULT_DATABASE}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true`, {
+            useNewUrlParser: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true,
+            keepAlive: true,
+            reconnectTries: Number.MAX_VALUE,
+            useMongoClient: true
+        })
         .then((result) => {
             callback(result)
         })
